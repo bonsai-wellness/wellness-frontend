f76915a0b0dc05b6c5ef29619e3c5675daab9ae5
sergioecv <sergiochz10@gmail.com>
2023-04-28 08:20:54 -0600

    Pagina reserva

--- src/app/pages/user/reservar/reservar.component.ts
+++ src/app/pages/user/reservar/reservar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Deporte } from 'src/app/models/deporte/deporte';
 
 @Component({
   selector: 'app-reservar',
@@ -12,4 +13,36 @@ export class ReservarComponent {
     document.title = this.title;
     console.log("Reserva")
   }
+
+  deportes: Deporte[]=[
+    {
+      deporte_id: 0,
+      created_at: "",
+      updated_at: "",
+      nombre: 'Tenis',
+      imagen: "../../../../assets/reservafondo.png",
+    },
+    {
+      deporte_id: 1,
+      created_at: "",
+      updated_at: "",
+      nombre: 'Futbol',
+      imagen: "../../../../assets/espaciosfondo.png",
+    },
+    {
+      deporte_id: 2,
+      created_at: "",
+      updated_at: "",
+      nombre: 'Basketball',
+      imagen: "../../../../assets/basket.jpeg",
+    },
+    {
+      deporte_id: 3,
+      created_at: "",
+      updated_at: "",
+      nombre: 'Volleyball',
+      imagen: "../../../../assets/volley.jpeg",
+    },
+
+  ];
 }

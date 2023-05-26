68d0a8d5e2cc2d4f63660a8d33e06d9f224c10cd
josecas <jmcg0901@gmail.com>
2023-05-25 21:16:25 -0600

    Added Google button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,30 +2,6 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MainComponent } from "./pages/auth/main/main.component";
 
-// const routes: Routes = [
-//   {
-//     path: "",
-//     loadChildren: () =>
-//     import("./auth/auth.module").then((m) => m.AuthModule),
-//   },
-//   {
-//     path: "login",
-//     loadChildren: () => 
-//       import("./auth/auth.module").then((m) => m.AuthModule),
-//   },
-//   {
-//     path: "admin",
-//     loadChildren: () =>
-//       import("./pages/admin/app-admin.module").then((m) => m.AppAdminModule),
-//   },
-//   {
-//     path: "user",
-//     loadChildren: () =>
-//       import("./pages/user/user-app/user-app.module").then((m) => m.UserAppModule
-//       ),
-//   },
-// ];
-
 const routes: Routes = [
   {
     path: "",
--- src/app/pages/user/user-app/user-app-routing.module.ts
+++ src/app/pages/user/user-app/user-app-routing.module.ts
@@ -3,19 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserAppComponent } from './user-app.component';
 import { ReservarDeporteComponent } from '../reservar-deporte/reservar-deporte.component';
 
-// const routes: Routes = [
-//   { path: '', component: UserAppComponent, 
-//     children: [
-//     // { path: '', pathMatch: 'full', redirectTo: '' },
-//     { path: '', loadChildren: () => import('../../../pages/user/home/home.module').then(m => m.HomeModule) },
-//     { path: 'reservar', loadChildren: () => import('../reservar-menu/reservar-menu.module').then(m => m.ReservarMenuModule)},
-//     { path: 'reservar/:id', loadChildren: () => import('../reservar-deporte/reservar-deporte.module').then(m => m.ReservarDeporteModule)},
-//     { path: 'res', loadChildren: () => import('../reservar-deporte/reservar-deporte.module').then(m => m.ReservarDeporteModule)},
-//     // { path: "**", redirectTo: "" },
-//     ],
-//   },
-
-// ];
 const routes: Routes = [
   {
     path: '',

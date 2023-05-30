85c4b985aa5314b90463b97a056d55c79c09558c
Paulina LH <65806675+paulinalh@users.noreply.github.com>
2023-05-30 07:48:24 -0600

    Added header component iin espacio details view

---
+++ src/app/components/admin/header-espacio/header-espacio.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppConstants } from 'src/app/app-constants';
+
+@Component({
+  selector: 'app-header-espacio',
+  templateUrl: './header-espacio.component.html',
+  styleUrls: ['./header-espacio.component.css']
+})
+export class HeaderEspacioComponent implements OnInit{
+  baseURL= AppConstants.baseURL;
+
+  @Input() name = ''
+  @Input() code = ''
+  @Input() open_at = ''
+  @Input() imagen = ''
+  @Input() details = ''
+  @Input() close_at = ''
+  @Input() time_max = ''
+  @Input() capacity = '';
+
+  constructor(private router:Router){
+  }
+
+  ngOnInit(): void {
+  }
+}

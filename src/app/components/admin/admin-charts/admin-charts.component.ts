c96864a1abb3aa0a674273ff15bc31238d4b72b5
emilianosca <emiliano_sanchez@criptext.com>
2023-06-02 08:03:46 -0600

    stats and graphs

---
+++ src/app/components/admin/admin-charts/admin-charts.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import Chart from 'chart.js/auto';
+
+@Component({
+  selector: 'app-admin-charts',
+  templateUrl: './admin-charts.component.html',
+  styleUrls: ['./admin-charts.component.css']
+})
+export class AdminChartsComponent {
+  radioValue = 'A';
+  public chart: any;
+  createChart(){
+  
+    this.chart = new Chart("MyChart", {
+      type: 'bar',
+      data: {
+        labels: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
+        datasets: [{
+          label: 'Entradas esta semana',
+          data: [1400, 1200, 1300, 1100, 1000, 900, 800],
+          borderColor: '#36A2EB',
+          backgroundColor: '#9BD0F5',    
+          borderWidth: 1
+        }]
+      },
+      options: {
+        animations: {
+          tension: {
+            duration: 1000,
+            easing: 'linear',
+            from: 1,
+            to: 0,
+            loop: true
+          }
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+            max: 2000,
+          }
+        },
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
+      }
+    });
+  }
+  ngOnInit(): void {
+    this.createChart();
+  }
+}
---
+++ src/app/components/admin/admin-people-card/admin-people-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-people-card',
+  templateUrl: './admin-people-card.component.html',
+  styleUrls: ['./admin-people-card.component.css']
+})
+export class AdminPeopleCardComponent {
+
+}

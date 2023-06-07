60f2224702a74e625b4ac49187e919c1e5caea90
sergioecv <sergiochz10@gmail.com>
2023-06-07 16:18:50 -0600

    clean code

--- src/app/components/user/home/anuncios/anuncios.component.ts
+++ src/app/components/user/home/anuncios/anuncios.component.ts
@@ -14,32 +14,9 @@ export class AnunciosComponent {
   constructor(private _apiservice: ApiserviceService){}
 
   arrAnuncios: any = [];
-  //  = [
-  // {
-    //   aviso_id: 0,
-    //   created_at: "../../../../assets/login-image.jpeg",
-    //   updated_at: "",
-    //   name: "Torneo Tochito",
-    //   description: "Canchas Escamilla",
-    //   image: '../../../../assets/wellness.jpeg',
-    //   is_active: true,
-    //   url: '',
-    // },
-    // {
-    //   aviso_id: 1,
-    //   created_at: '../../../../../assets/anunciod.png',
-    //   updated_at: "",
-    //   name: "Torneo Tochito",
-    //   description: "Canchas Escamilla",
-    //   image: '../../../../assets/tecfoto.jpeg',
-    //   is_active: true,
-    //   url: '',
-    // } 
-  // ];
 
   ngOnInit(){
     this._apiservice.getAllActiveAnuncios().subscribe( (res) =>{
-      console.log("anuncios", res);
       this.arrAnuncios = res;
         });
   }

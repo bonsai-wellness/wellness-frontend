4d5015f81253e1e9854c0ead87fcf743dfe4ee38
emilianosca <emiliano_sanchez@criptext.com>
2023-06-14 00:44:01 -0600

    works :)

--- src/app/components/admin/admin-people-card/admin-people-card.component.ts
+++ src/app/components/admin/admin-people-card/admin-people-card.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-admin-people-card',
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 })
 
 export class AdminPeopleCardComponent {
-  API: string = "http://localhost:8000/api";
-  // API: string = "https://bonsai-rest.azurewebsites.net/api";
+  // API: string = "http://localhost:8000/api";
+  API: string = "https://bonsai-rest.azurewebsites.net/api";
 
   constructor(private _http: HttpClient) {}
 
@@ -25,113 +25,55 @@ export class AdminPeopleCardComponent {
     return this._http.get(this.API + "/wellness-log", { headers });
   }
 
-  jsonData:any = []
+  
+  jsonData : any 
 
   getData(): void {
       this.fetchData().subscribe((data: any) => {
-        console.log('this is the data ' , data);
-        this.jsonData = data;
-        const actual = Number(data.aforo_actual);
-        const max = Number(data.capacidad_max)
-      });
-      this.exportCsv() 
+      this.jsonData = data;
+      console.log('this is data ' , this.jsonData)
+      this.exportToCsv()
+      }); 
 	}
 
+  exportToCsv(): void {
+    const csvContent = this.convertArrayToCsv(this.jsonData);
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement('a');
+    const url = URL.createObjectURL(blob);
 
-  title = 'csv export';
-
-  // jsonData : any = [{
-  //   name : 'Berlin',
-  //   age : '45',
-  //   country : 'Spain',
-  //   phone : '896514326'
-  // },
-  // {
-  //   name : 'Professor',
-  //   age : '42',
-  //   country : 'spain'
-  // },
-  // {
-  //   name : 'Tokyo',
-  //   age : '35',
-  //   phone : '854668244'
-  // },
-  // {
-  //   name : 'Helsinki',
-  //   phone : '35863297'
-  // }];
-
-  exportCsv() {
-    this.jsonData = JSON.stringify(this.jsonData);
-    console.log(this.jsonData , ' that was jsonData')
-    this.downloadFile(this.jsonData);
+    link.setAttribute('href', url);
+    link.setAttribute('download', 'data.csv');
+    link.style.visibility = 'hidden';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 
-  downloadFile(data: string, filename = 'data') {
-    let arrHeader = ["updated_at", "type", "wellness_log_id",];
-    let csvData = this.ConvertToCSV(data, arrHeader);
-    console.log(csvData)
-    let blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
-    let dwldLink = document.createElement("a");
-    let url = URL.createObjectURL(blob);
-    let isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
-    if (isSafariBrowser) {  //if Safari open in new window to save file with random filename.
-      dwldLink.setAttribute("target", "_blank");
+  convertArrayToCsv(data: any[]): string {
+    const csvRows: string[] = [];
+
+    // Get the headers from the first element
+    const headers = Object.keys(data[0]);
+
+    // Add the headers row to the CSV
+    csvRows.push(headers.join(','));
+
+    // Loop through the data and convert each object to a CSV row
+    for (const item of data) {
+      const values = headers.map(header => this.escapeCsvValue(item[header]));
+      csvRows.push(values.join(','));
     }
-    dwldLink.setAttribute("href", url);
-    dwldLink.setAttribute("download", "sample.csv");
-    dwldLink.style.visibility = "hidden";
-    document.body.appendChild(dwldLink);
-    dwldLink.click();
-    document.body.removeChild(dwldLink);
+
+    // Join all the rows into a single CSV string
+    return csvRows.join('\n');
   }
 
-  ConvertToCSV(objArray: string, headerList: string[]) {
-    console.log(objArray);
-    console.log(headerList);
-    let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-    let str = '';
-    let row = 'S.No,';
-
-    let newHeaders = ["updated_at", "type", "wellness_log_id",];
-
-    for (let index in newHeaders) {
-      row += newHeaders[index] + ',';
-    }
-    row = row.slice(0, -1);
-    str += row + '\r\n';
-    for (let i = 0; i < array.length; i++) {
-      let line = (i + 1) + '';
-      for (let index in headerList) {
-        let head = headerList[index];
-
-        line += ',' + this.strRep(array[i][head]);
-      }
-      str += line + '\r\n';
-    }
-    return str;
+  escapeCsvValue(value: any): string {
+    // Escape double quotes and wrap the value in double quotes if necessary
+    const escapedValue = String(value).replace(/"/g, '""');
+    return `"${escapedValue}"`;
   }
-
-  strRep(data: any) {
-    if(typeof data == "string") {
-      let newData = data.replace(/,/g, " ");
-       return newData;
-    }
-    else if(typeof data == "undefined") {
-      return "-";
-    }
-    else if(typeof data == "number") {
-      return  data.toString();
-    }
-    else {
-      return data;
-    }
-  }
-
-
+}
   
 
-
-
-
-}

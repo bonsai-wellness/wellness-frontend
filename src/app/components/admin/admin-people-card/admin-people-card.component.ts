4965adc0d89f9fad4bb452b2c8dcb40bd8b18854
emilianosca <emiliano_sanchez@criptext.com>
2023-06-15 23:34:29 -0600

    fixed bugs

--- src/app/components/admin/admin-people-card/admin-people-card.component.ts
+++ src/app/components/admin/admin-people-card/admin-people-card.component.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Component, ViewChild, OnDestroy, OnInit } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
 
+
 @Component({
   selector: 'app-admin-people-card',
   templateUrl: './admin-people-card.component.html',
@@ -22,67 +23,88 @@ export class AdminPeopleCardComponent {
 
   fetchData() {
     const headers = this.authHeader();
-    return this._http.get(this.API + "/wellness-gym/1", { headers });
+    return this._http.get(this.API + "/wellness-gym/1", { headers })
   }
 
-  // fetchReservations() {
-  //   const headers = this.authHeader();
-  //   return this._http.get(this.API + "reservation/today/1", { headers });
-  // }
-
   
-  jsonData : any 
+  jsonData : any  = []; 
 
   getData(): void {
       this.fetchData().subscribe((data: any) => {
-      this.jsonData = data;
+      this.jsonData = data
+  
       }); 
 	}
 
-  // getReservations(): void {
-  //   this.fetchReservations().subscribe((data: any) => {
-  //   this.jsonData = data;
-  //   });
-  // }
-
-
   ngOnInit(): void {
     this.getGym();
-    // this.getReservations();
+    this.getReservations();;
     setInterval(() => {
       this.getGym();
-      // this.getReservations();
+      this.getReservations();
     }, 5000);
   }
 
-  getGym(): void {
-    this.getData();
-    // this.getReservations();
+  jsonDataArray : any = [];
 
-    let aforoActual = 0;
-    let aforoMax = 0;
-    if (this.jsonData) {
-      aforoActual = this.jsonData.aforo_actual;
-      aforoMax = this.jsonData.capacidad_max;
+  getGym(): void {
+    let porcentaje = 0;
+    let aforoActualString = "";
+   
+    // look for the data, and if it is not found, set it to 0
+    if (this.jsonData.aforo_actual == null) {
+      aforoActualString = "0";
+    } else {
+      aforoActualString = this.jsonData.aforo_actual;
     }
 
-    
-    
-    let aforoActualHTML:any = document.getElementById("aforoActual");
-    aforoActualHTML.innerHTML = aforoActual.toString();
+    // look for the data, and if it is not found, set it to 0
+    if (this.jsonData.capacidad_max == null) {
+      porcentaje = 0;
+    } else {
+      porcentaje = (this.jsonData.aforo_actual / this.jsonData.capacidad_max) * 100;
+    }
 
-    let porcentaje = Math.round((aforoActual / aforoMax) * 100);
-    let porcentajeHTML:any = document.getElementById("porcentaje");
-    porcentajeHTML.innerHTML = porcentaje.toString() + "%"; 
 
-    // let reservations = this.jsonData.reservations;
     
+    this.jsonDataArray = [
+      {
+        aforo_actual: aforoActualString.toString(),
+        porcentaje: porcentaje.toString(),
+      } 
+    ]
+    this.getData();
+
   }
 
 
 
+    // function that fetch reservations from the API
+    fetchDataRes() {
+      const headers = this.authHeader();
+      return this._http.get(this.API + "/reservation/today", { headers });
+    }
+  
 
+    jsonDataRes : any
+    jsonDataResArray : any = [];
+    getReservations(): void {
+      this.fetchDataRes().subscribe((data: any) => {
+      this.jsonDataRes = data;
+      }
+      );
+      let length = 0;
+      if (this.jsonDataRes != null) {
+        length = this.jsonDataRes.length;
+      }
 
+      this.jsonDataResArray = [
+        {
+          reservaciones: length.toString()
+        }
+      ]
+    }
+  
 
 
 }
